0df2a09d318abbb3ee13e1e8a6d1c61d602369fc
dillon Xie <dillon.xie@oocl.com>
2019-12-04 08:26:37 +0800

    first

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '" https://www.easy-mock.com/mock/5de5f0ad0800c56348015924"',
+})
